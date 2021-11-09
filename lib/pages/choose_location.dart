d1620c281e6ce0a88e1ff7365ff8cee287b89840
mukimahrizky <mukimahrizky@gmail.com>
2021-11-09 17:52:44 +0800

    get data from API

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,32 +9,8 @@ class ChooseLocaton extends StatefulWidget {
 
 class _ChooseLocatonState extends State<ChooseLocaton> {
 
-  int counter = 0;
-  
-  void getData() async {
-
-    // simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'yoshi';
-    });
-
-    // simulate network request to get bio of the username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'vega, musician & egg collector';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('hei');
-  }
   @override
   Widget build(BuildContext context) {
-    print('build function ran');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -42,14 +18,6 @@ class _ChooseLocatonState extends State<ChooseLocaton> {
         title: Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
-      ),
-      body: RaisedButton(
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
-        },
-        child: Text('counter is $counter'),
       ),
     );
   }
