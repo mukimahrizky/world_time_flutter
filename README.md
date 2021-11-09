43fc39443788560ffe922b2eee623b2ea4a2d923
mukimahrizky <mukimahrizky@gmail.com>
2021-11-09 18:27:44 +0800

    world time API

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 https://www.youtube.com/watch?v=WghpP9W2vXo&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ&index=23
 https://pub.dev/packages/http/install
 https://jsonplaceholder.typicode.com/
+https://worldtimeapi.org/timezone
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,18 +11,32 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    var url = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
-    Response response = await get(url);
+  void getTime() async {
+
+    // make the Request
+    var timezoneMakassar = Uri.https('worldtimeapi.org', '/api/timezone/Asia/Makassar');
+    Response response = await get(timezoneMakassar);
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    //print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    print(datetime);
+    //print(offset);
+
+    // crate DateTime object
+    DateTime now = DateTime.parse(datetime);
+    print(now);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
