b0543bd964328be93f22c10ea8854c9a1cd34e2d
mukimahrizky <mukimahrizky@gmail.com>
2021-11-09 21:57:45 +0800

    list view builder

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_flutter/services/world_time.dart';
 
 class ChooseLocaton extends StatefulWidget {
   const ChooseLocaton({Key? key}) : super(key: key);
@@ -8,6 +9,17 @@ class ChooseLocaton extends StatefulWidget {
 }
 
 class _ChooseLocatonState extends State<ChooseLocaton> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +31,25 @@ class _ChooseLocatonState extends State<ChooseLocaton> {
         centerTitle: true,
         elevation: 0,
       ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  print(locations[index].location);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              )
+            ),
+          );
+        },
+      )
     );
   }
 }
